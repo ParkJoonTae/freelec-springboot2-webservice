3acf7a42751c1a4b604ba237d0c6e5bc6e7fd44b
pjt <ksi05298@gmail.com>
2022-06-14 16:12:56 +0900

    0614 commit

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.config.auth.dto;
+
+import com.jojoldu.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    // SessionUser을 새로 만들지 않고 User그대로 쓰면 에러. User는 Entity클래스이기 때문.
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
---
+++ src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
@@ -0,0 +1,15 @@
+package com.jojoldu.book.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {     // 사용자의 권한을 관리하는 enum클래스
+    GUEST("ROLE_GUEST", "손님"),  // 권한 앞에 ROLE_이 있어야만 한다.
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+
+}
