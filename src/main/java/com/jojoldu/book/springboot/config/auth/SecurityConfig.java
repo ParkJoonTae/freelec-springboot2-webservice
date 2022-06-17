d5a250149434a5fd06d7c7984d7634761c81a183
pjt <ksi05298@gmail.com>
2022-06-17 14:41:45 +0900

    0617 profile api 추가

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().authorizeRequests()
                 //URL별 권한 관리를 설정하는 옵션의 시작점
                 .antMatchers("/", "/css/**", "/images/**",
-                        "/js/**", "/h2-console/**").permitAll()
+                        "/js/**", "/h2-console/**","/profile").permitAll()
                 //권한 관리 대상 지정. "/"등 지정된 URL들은 permitAll을 통해 전체열람권한.
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name()).anyRequest().authenticated().and()
                 // 설정된 값들 이외 나머지 URL들을 나타낸다. authenticated으로 나머지 URL들은 모두 인증된 사용자들에게만 허용
